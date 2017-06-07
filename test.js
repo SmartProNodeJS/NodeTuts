45a30e658a2032eed041b189bffcb6e08ecdb0e5
lephuchien <lephuchien@users.noreply.github.com>
2017-06-07 21:01:17 +0700

    Update test.js

--- test.js
+++ test.js
@@ -4,5 +4,11 @@ var httpServer = http.createServer( function(request, response) {
     response.setHeader("Content-Type", "text/html");
     response.end("<html><title>Hello</title><body><h1>Hello, World</h1></body></html>");
 }).listen(8080, function(){
-    console.log("Server is listening on port 8080");
+    console.log("Server is listening fgfghgfhgfhghfghghgfhgfhfghgfhfghgfh
+                gfhf
+                hgfhgh
+                gf
+                hghgfhf
+                gh
+                fghfghfghgfhhgfhgfhgfon port 8080");
 });
