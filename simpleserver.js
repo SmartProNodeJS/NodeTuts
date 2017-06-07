1a8eb8df3622bffc601c870df00e69548f544626
sonnguyen79vn <sonnguyen79vn@gmail.com>
2017-06-07 20:54:08 +0700

    sqqsqs
    qsqsqq

--- simpleserver.js
+++ simpleserver.js
@@ -1,6 +1,6 @@
 var http = require("http");
 var httpServer = http.createServer( function(request, response) {
-    response.statusCode = 200;
+    response.statusCode = 2001;
     response.setHeader("Content-Type", "text/html");
     response.end("<html><title>Hello</title><body><h1>Hello, World</h1></body></html>");
 }).listen(8080, function(){
