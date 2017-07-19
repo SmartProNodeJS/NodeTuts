600dd0d4370db5f8e2863c8964c17ab50d7f1cd7
Hồ Chí Thanh <hochithanh.cntt@gmail.com>
2017-07-19 17:38:37 +0700

    commit new code

--- app.js
+++ app.js
@@ -26,6 +26,14 @@ app.use(function(request, res, next){
 });
 var playerRoute = require('./routes/player.js');
 app.use("/player", playerRoute);
+var teamRoute = require('./routes/team.js');
+app.use("/team", teamRoute);
+var matchRoute = require('./routes/match.js');
+app.use("/match", matchRoute);
+var leagueRoute = require('./routes/league.js');
+app.use("/league", leagueRoute);
+var categoryRoute = require('./routes/category.js');
+app.use("/category", categoryRoute);
 // Create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 app.use(express.static("public/"));
@@ -35,7 +43,7 @@ app.set("view engine","pug");
 
 app.get('/', urlencodedParser, function (req, res) {
     menu_items.forEach(function(it){
-        console.log(it.name+" is "+it.active);
+        //console.log(it.name+" is "+it.active);
         it.active = (it.name=='home')?"active":"";
     });
     res.render("index",{"menu_items":menu_items});
@@ -43,17 +51,31 @@ app.get('/', urlencodedParser, function (req, res) {
 
 var Sports = require('./models/sports');
 
-Sports.findAll().then(function(sports){
-    console.log("Sports: "+JSON.stringify(sports));
-}).catch(function(err){
-    console.log("Error: "+err);
+app.get('/sports',function(req, res) {
+    Sports.findAll().then(function(sports){
+    //console.log("Sports: "+JSON.stringify(sports));
+    res.status(200).json(sports);
+    }).catch(function(err){
+        console.log("Error: "+err);
+    });
+});
+app.get('/sport/:name',function(req, res) {
+    Sports.findByName(req.params.name).then(function(sports){
+        console.log("Sports with name (Football): "+JSON.stringify(sports));
+        res.status(200).json(sports[0]);
+    }).catch(function(err){
+        console.log("Error: "+err);
+    }); 
 });
 
-Sports.findByName("Football").then(function(sports){
-    console.log("Sports with name (Football): "+JSON.stringify(sports));
-}).catch(function(err){
-    console.log("Error: "+err);
-}); 
+app.post('/sport',urlencodedParser, function(req, res) {
+    Sports.findByName(req.body.sport_name).then(function(sports){
+        console.log("Sports with name("+req.body.sport_name+"): "+JSON.stringify(sports));
+        res.status(200).json(sports[0]);
+    }).catch(function(err){
+        console.log("Error: "+err);
+    }); 
+});
 
 var ioHttp = http.Server(app);
 
@@ -86,8 +108,33 @@ io.on("connection", function(socket){
     sockets.push({"user":"", "socket":socket});
 });
 
-var server = ioHttp.listen(8081, function () {
+var handle404Pages = function handle404(req, res, next){
+  console.log("Response Type: "+ req.accepts('html'));
+    menu_items.forEach(function(it){
+        it.active = (it.name=='home')?"active":"";
+    });
+  res.status(404);
+  // respond with html page
+  if (req.accepts('html')) {
+    res.render('404', {"menu_items":menu_items, url: req.url });
+    return;
+  }
+
+  // respond with json
+  if (req.accepts('json')) {
+    res.send({ error: 'Not found' });
+    return;
+  }
+  // default to plain-text. send()
+  res.type('txt').send('Not found');    
+}
+
+app.use(handle404Pages);
+
+var server = ioHttp.listen(8082, function () {
     var host = server.address().address,
     port = server.address().port;
     console.log("Example app listening at http://%s:%s", host, port);
 })
+
+module.exports = app;
